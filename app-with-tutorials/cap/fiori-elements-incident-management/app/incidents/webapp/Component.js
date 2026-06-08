e5c62e5e23088a88e4b379ac8f3d0fbbb11cb781
John Long <john.long@sap.com>
2026-06-08 10:38:54 +0100

    fix: restore app-with-tutorials folder
    Restores the app-with-tutorials directory removed in 3051108.
    Folder contains non-cap/sapui5 and cap tutorial samples referenced
    in SAP tutorials and documentation.

---
+++ app-with-tutorials/cap/fiori-elements-incident-management/app/incidents/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("sap.fe.demo.incidents.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
---
+++ app-with-tutorials/non-cap/sapui5/basic/manage-products/dist/controller/View1.controller.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(n){"use strict";return n.extend("sap.btp.sapui5.controller.View1",{onInit:function(){}})});
